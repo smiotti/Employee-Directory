2e2f3c3d8ab47f604676d099fbb9842f041f4c1a
Miotti <210061319@LOGON.DS.GE.COM>
2018-10-26 13:44:35 -0400

    Updates to HTML and JS files.

--- app.js
+++ app.js
@@ -65,6 +65,9 @@ const showVerifyInput = function () {
 // Showing the Update function button and fields
 const showUpdateInput = function () {
     $(`.inputFields`).show();
+    $(`#nameInp`).show();
+    $(`#officeNumInp`).show();
+    $(`#phoneNumInp`).show();
     $(`#updateButton`).show();
     $(`#deleteButton`).hide();
     $(`#addButton`).hide();
@@ -75,8 +78,7 @@ const showUpdateInput = function () {
 
 
 
-
-//  // When the View button is pressed or 'render is called, display all of the employee information in `employeeList` individually in paragraph tags to the div with the class `content`.
+ // When the View button is pressed or 'render is called, display all of the employee information in `employeeList` individually in paragraph tags to the div with the class `content`.
 const render = function () {
     $('.content').empty();
     $('.verifyId').empty();
@@ -139,19 +141,23 @@ $('#addButton').on('click', getInputVal);
 
 //  When the delete button is pressed, delete the element from employeeList that matches the name the user entered in the input field. Re-render the list.
 const removeName = function () {
+    let onEmployeetList = false;
     // Gathering user input.
     const nameVal = $('#nameInp').val();
-    // const officeVal = $('#officeNumInp').val();
-    // const phoneVal = $('#phoneNumInp').val();
     
-
+    // If selected employee name is in the list, delete the employee record.
     for (let i = 0; i < employeeList.length; i++) {
         if (employeeList[i].name === nameVal) {
           employeeList.splice(i, 1);
+          onEmployeetList = true;
           
         }    
     }   
   
+    // If selected employee name not in the list, alert user.
+    if (onEmployeetList == false) {
+        alert(nameVal + " is not in the employee list!");
+        }
 
     // After performing our actions, clear the input values and re-render the list
     $('#nameInp').val('');
@@ -214,15 +220,44 @@ $('#verifyButton').on('click', verifyName);
 //  When the Update button is pressed, allows the user to input name, office number, and phone number and then update the office number and phone number 
 //  of the employee that matches the input name, and then renders the updated employee list.
 const updateName = function () {
+    let onEmployeetList = false;
+
+    // const updateEmployee = {
+    //     name: '',
+    //     officeNum: '',
+    //     phoneNum: ''
+    // };
+
+    // Gathering user input.
     const nameVal = $('#nameInp').val();
+    const officeVal = $('#officeNumInp').val();
+    const phoneVal = $('#phoneNumInp').val();
+    
+    // Assigning user input to updateEmployee object.
+    // updateEmployee.name = nameVal;
+    // updateEmployee.officeNum = officeVal ;
+    // updateEmployee.phoneNum = phoneVal;
 
-    // We use the indexOf method to find the index of the input name
-    // Then we use splice to remove 1 element, starting with that index
+    // If selected empoyee is on the list, update with new employee infomation.
+    for (let i = 0; i < employeeList.length; i++) {
+        if (employeeList[i].name === nameVal) {
+            employeeList[i].name = nameVal;
+            employeeList[i].officeNum = officeVal;
+            employeeList[i].phoneNum = phoneVal;
+            onEmployeetList = true;
+        }
+    }
 
-    // employeeList.splice(employeeList.indexOf(nameVal), 1);
+    // If selected employee name not in the list, alert user.
+        if (onEmployeetList == false) {
+        alert(nameVal + " is not in the employee list!");
+     }
+          
 
     // After performing our actions, clear the input values and re-render the list
-    $('#nameInp').val('');
+     $('#nameInp').val('');
+     $('#officeNumInp').val('');
+     $('#phoneNumInp').val('');
     render();
 
 }
@@ -232,34 +267,3 @@ $('#updateButton').on('click', updateName);
 
 
 
-
-
-
-
-
-
-// Please watch this video on single page applications _before_ getting started: <https://youtu.be/_gDMQzNVxQA>
-
-// For this assignment you will be building an online employee directory for your company using your DOM manipulation library and Bootstrap.
-
-// There is an array of employees in the `employeeList.js` file. Note that there is only 1 html file. This is a single page application. You will need to hide and show sections as necessary.
-
-// Begin by creating the `app.js` file, and linking to it from `index.html`.
-
-// In `app.js` you will need to implement all of the following functionality:
-
-// 1. A `View` option that displays all employee info:
-
-// ![view](../images/print.png)
-
-// 2. An `Add` option that allows users to input name, office number, and phone number and then renders the updated employee list.
-
-// ![add](../images/add.png)
-
-// 3. A `Verify` option that allows users to input a name and renders `yes` if the employee exists and `no` otherwise.
-
-// ![verify](../images/verify.png)
-
-// 4. An `Update` option that allows the user to input name, office number, and phone number and updates the office number and phone number of the employee that matches the input name, and then renders the updated employee list.
-
-// 5. A `Delete` option that deletes the employee with the matching name and then renders the updated employee list.
